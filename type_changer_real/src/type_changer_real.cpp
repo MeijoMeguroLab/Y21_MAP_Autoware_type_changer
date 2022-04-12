84d40d042bc2ff7e90a9fe0e4e46970c32297b87
YutaHoda <170447064@ccalumni.meijo-u.ac.jp>
2022-04-12 14:31:34 +0900

    Support for reverse_imu

--- type_changer_real/src/type_changer_real.cpp
+++ type_changer_real/src/type_changer_real.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <velodyne_msgs/VelodyneScan.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Imu.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <math.h>
 
@@ -12,6 +12,11 @@ ros::Publisher packets_pub;
 static std::string sub_twist_topic_name;
 static std::string pub_twist_topic_name;
 ros::Publisher twist_pub;
+
+static std::string sub_imu_topic_name;
+static std::string pub_imu_topic_name;
+ros::Publisher imu_pub;
+bool reverse_imu = false;
 
 void packetsCallback(const velodyne_msgs::VelodyneScan::ConstPtr& msg) {
   velodyne_msgs::VelodyneScan velodyne_packets;
@@ -27,25 +32,44 @@ void twistCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
   twist_pub.publish(twist);
 }
 
+void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+  sensor_msgs::Imu imu;
+  imu = *msg;
+  if(reverse_imu){
+    imu_pub.publish(imu);
+  }else{
+    imu.angular_velocity.z =  -1 * imu.angular_velocity.z;
+    imu_pub.publish(imu);
+  }
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "type_changer_real");
   ros::NodeHandle n;
 
   n.getParam("sub_lidar_topic_name",sub_lidar_topic_name);
   n.getParam("sub_twist_topic_name",sub_twist_topic_name);
+  n.getParam("sub_imu_topic_name",sub_imu_topic_name);
   n.getParam("pub_lidar_topic_name",pub_lidar_topic_name);
   n.getParam("pub_twist_topic_name",pub_twist_topic_name);
+  n.getParam("pub_imu_topic_name",pub_imu_topic_name);
+  n.getParam("reverse_imu",reverse_imu);
 
   std::cout<< "sub_lidar_topic_name "<<sub_lidar_topic_name<<std::endl;
   std::cout<< "sub_twist_topic_name "<<sub_twist_topic_name<<std::endl;
+  std::cout<< "sub_imu_topic_name "<<sub_imu_topic_name<<std::endl;
   std::cout<< "pub_lidar_topic_name "<<pub_lidar_topic_name<<std::endl;
   std::cout<< "pub_twist_topic_name "<<pub_twist_topic_name<<std::endl;
+  std::cout<< "pub_imu_topic_name "<<pub_imu_topic_name<<std::endl;
+  std::cout<< "reverse_imu "<<reverse_imu<<std::endl;
 
   ros::Subscriber packets_sub = n.subscribe(sub_lidar_topic_name, 10, packetsCallback);
   ros::Subscriber twist_sub = n.subscribe(sub_twist_topic_name, 10, twistCallback);
+  ros::Subscriber imu_sub = n.subscribe(sub_imu_topic_name, 10, imuCallback);
 
   packets_pub = n.advertise<velodyne_msgs::VelodyneScan>(pub_lidar_topic_name, 10);
   twist_pub = n.advertise<geometry_msgs::TwistStamped>(pub_twist_topic_name, 10);
+  imu_pub = n.advertise<sensor_msgs::Imu>(pub_imu_topic_name, 10);
 
   ros::spin();
   return 0;
